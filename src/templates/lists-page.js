c14ca9549d5650e4655bb3c85b72161437a41bef
krishnaprasadk1 <53489162+krishnaprasadk1@users.noreply.github.com>
2019-08-29 14:58:56 +0530

    Update lists-page.js

--- src/templates/lists-page.js
+++ src/templates/lists-page.js
@@ -13,6 +13,21 @@ const ListsPage = ( {data} ) => {
         <p>{post.frontmatter.description}</p>
         {post.html}
       </div>
+        
+        <section className="section section--gradient">
+          <div className="container">
+            <div className="columns">
+              <div className="column is-10 is-offset-1">
+                <div className="section">
+                  <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
+                    {post.frontmatter.title}
+                  </h2>
+                  <PageContent className="content" content={post.html} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
     );
 };
 
