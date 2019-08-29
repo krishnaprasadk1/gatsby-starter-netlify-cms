3b1059f400e68a7f5ec5fd32e16e8e120e6a2a9b
krishnaprasadk1 <53489162+krishnaprasadk1@users.noreply.github.com>
2019-08-29 15:12:48 +0530

    Update lists-page.js

--- src/templates/lists-page.js
+++ src/templates/lists-page.js
@@ -8,13 +8,26 @@ const ListsPage = ( {data} ) => {
     const { markdownRemark: post } = data;
 
     return(
-      <div className="section">
+      <!--<div className="section">
         <h2 className="title is-size-3 has-text-weight-bold is-bold-light">{post.frontmatter.title}</h2>
         <p>{post.frontmatter.description}</p>
         {post.html}
-      </div>
+      </div>-->
         
-        
+        <section className="section section--gradient">
+          <div className="container">
+            <div className="columns">
+              <div className="column is-10 is-offset-1">
+                <div className="section">
+                  <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
+                    {post.frontmatter.title}
+                  </h2>
+                  <PageContent className="content" content={post.html} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
     );
 };
 
